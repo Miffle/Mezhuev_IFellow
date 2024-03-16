d5e6b7338e1433dd476356fc9c177422fc0fdfc4
Mezhuev Roman <miffle1337@yandex.ru>
2024-03-16 10:33:50 +0300

    HW 5

--- src/test/java/hooks/WebHooks.java
+++ src/test/java/hooks/WebHooks.java
@@ -6,6 +6,9 @@ import io.restassured.filter.log.ResponseLoggingFilter;
 import org.junit.jupiter.api.BeforeEach;
 
 public class WebHooks {
+    /**
+     * Фильтры добавляют логирование запросов и ответов
+     */
     @BeforeEach
     public void beforeEach() {
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
